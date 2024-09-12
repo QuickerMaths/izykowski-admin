27f47d7fb29cf8a62cd872334a7c8a9a20704315
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-09-12 14:48:19 +0200

    added boiler code for adminjs

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
-const message: string = "Hello, TypeScript!";
-console.log(message);
+import AdminJS from 'adminjs'
+import AdminJSExpress from '@adminjs/express'
+import express from 'express'
+
+const PORT = 3000
+
+const start = async () => {
+  const app = express()
+
+  const admin = new AdminJS({})
+
+  const adminRouter = AdminJSExpress.buildRouter(admin)
+  app.use(admin.options.rootPath, adminRouter)
+
+  app.listen(PORT, () => {
+    console.log(`AdminJS started on http://localhost:${PORT}${admin.options.rootPath}`)
+  })
+}
+
+start()

7800b718a787b8ff34c8d1b28e444c95891b9501
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-09-13 16:32:06 +0200

    refactor

--- api/index.ts
+++ api/index.ts
@@ -3,29 +3,37 @@ import AdminJSExpress from '@adminjs/express';
 import express from 'express';
 import * as AdminJSMongoose from '@adminjs/mongoose';
 import mongoose from 'mongoose';
-import { Image } from './entities/Image.js';
+import { Image } from '../entities/Image.js';  // Adjust the path if necessary
 import dotenv from 'dotenv';
 
 dotenv.config();
-
-const PORT = 3000;
 
 AdminJS.registerAdapter({
     Database: AdminJSMongoose.Database,
     Resource: AdminJSMongoose.Resource,
 });
+
+const app = express();
+
 const start = async () => {
-    const app = express();
-    await mongoose.connect(process.env.DB_URI as string);
-    const adminOptions = {
-        resources: [Image],
-    };
-    const admin = new AdminJS(adminOptions);
-    const adminRouter = AdminJSExpress.buildRouter(admin);
-    app.use(admin.options.rootPath, adminRouter);
-    app.get('/', (req, res) => res.send('Express on Vercel'));
-    app.listen(PORT, () => {
-        console.log('http://localhost:3000');
-    });
+    try {
+        await mongoose.connect(process.env.DB_URI as string);
+
+        const adminOptions = {
+            resources: [Image],
+        };
+        const admin = new AdminJS(adminOptions);
+        const adminRouter = AdminJSExpress.buildRouter(admin);
+
+        app.use(admin.options.rootPath, adminRouter);
+
+        app.get('/', (_req, res) => res.send('Express on Vercel'));
+    } catch (error) {
+        console.error('Error starting the server:', error);
+    }
 };
+
 start();
+
+export default app;
+

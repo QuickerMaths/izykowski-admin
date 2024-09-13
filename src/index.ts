108fab4b6de53081e48274f066c7919bd37a44d7
QuickerMaths <mateusz.szalowicz@gmail.com>
2024-09-13 12:20:32 +0200

    basic adminjs setup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import AdminJS from 'adminjs'
 import AdminJSExpress from '@adminjs/express'
 import express from 'express'
+import * as AdminJSMongoose from '@adminjs/mongoose';
+import mongoose from 'mongoose';
 
 const PORT = 3000
 
+AdminJS.registerAdapter({
+    Database: AdminJSMongoose.Database,
+    Resource: AdminJSMongoose.Resource
+});
+
 const start = async () => {
+    await mongoose.connect(process.env.DB_URI!);
   const app = express()
 
   const admin = new AdminJS({})
